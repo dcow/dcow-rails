646745c366ceef7ed00bd364db640d6c1a62ea27
dcow <dcow90@gmail.com>
2012-08-28 18:29:16 +0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DcowRails::Application.config.secret_token = 'e0c8e41eb635ccfc19e308bd120fce0e39053cd64d5752d5fe049254f3aad5d82fd9c6246c5a38342c75dd6573b876b26cce19ea264167b821b2ce30bc9b60f0'
